5418be46dc264e69bb800f7ee94f5757567b4fef
gopae02 <gopae02@gmail.com>
2020-09-28 23:14:42 +0900

    # 파이썬 기본 환경 셋팅

---
+++ chap01/desc0.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 15 16:24:55 2019
+
+@author: Sim
+"""
+
+import pandas as pd
+import numpy as np
+
+#Create a Dictionary of series
+d = {'Name':pd.Series(['Tom','James','Ricky','Vin','Steve','Smith','Jack',
+                        'Lee','David','Gasper','Betina','Andres']),
+   'Age':pd.Series([25,26,25,23,30,29,23,34,40,30,51,46]),
+   'Rating':pd.Series([4.23,3.24,3.98,2.56,3.20,4.6,3.8,3.78,2.98,4.80,4.10,3.65])}
+
+#Create a DataFrame
+df = pd.DataFrame(d)
+print (df)
+df.head()
